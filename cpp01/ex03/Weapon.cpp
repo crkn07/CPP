9f8079a359e49a3c1cf32e43812272e8ec42792d
crkn07 <cristiantorres07@hotmail.com>
2024-05-28 16:46:23 +0200

    New exercises in CPP01/ex03

--- cpp01/ex03/Weapon.cpp
+++ cpp01/ex03/Weapon.cpp
@@ -6,7 +6,7 @@
 /*   By: crtorres <crtorres@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 18:19:35 by crtorres          #+#    #+#             */
-/*   Updated: 2024/05/28 16:23:37 by crtorres         ###   ########.fr       */
+/*   Updated: 2024/05/28 16:38:38 by crtorres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,15 @@ Weapon:: Weapon(std::string type){
     this->type = type;
 }
 
+void Weapon::setType(std::string type){
+    this->type = type;
+}
 
+std::string const &Weapon::getType(){
+    return this->type;
+}
+
+Weapon::~Weapon(){
+    std::cout << "Weapon "<< type <<" was destroyed" << std::endl;
+    return;
+}
